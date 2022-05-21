830c3950ef4268521a86e3956a556b0a7be107bb
abhishektambewagh <105218803+abhishektambewagh@users.noreply.github.com>
2022-05-21 11:35:02 +0530

    Create shell-script.sh
    This script is exporting files to windows' local directory and uploading those files into the s3 bucket

---
+++ shell-script.sh
@@ -0,0 +1,9 @@
+echo 'mysql-u root -pPravin97@vp -h xxx.x.x.x < /home/pravin/sample/dim_policy.sql'
+
+echo 'mysql-u root -pPravin97@vp -h xxx.x.x.x < /home/pravin/sample/dim_product.sql'
+
+echo  `aws configure set aws_access_key_id default_access_key`
+echo  `aws configure set aws_secret_access_key default_secret_key`
+echo  `aws configure set default.region us-east-1`
+
+echo `aws s3 sync "/mnt/c/ProgramData/mysql/MySql server 8.0" s3://test-bucket-snowflake-lab/ --region "us-east-1"`
